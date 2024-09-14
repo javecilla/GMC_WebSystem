26a531ab2c9a1300666c30cc76f51524bccce240
Jerome Avecilla <jeromesavc@gmail.com>
2024-09-14 09:27:32 +0800

    fix build assets for  production error cause mime type

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
       ],
       refresh: true,
       publicDirectory: 'public',
-      buildDirectory: 'build', // Changed this line
+      buildDirectory: 'build',
     }),
     vue({
       template: {
@@ -37,19 +37,20 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, 'resources/assets'),
       vue: 'vue/dist/vue.esm-bundler.js',
       '~bootstrap': 'bootstrap',
+      '~vite': path.resolve(__dirname, 'public/build'),
     },
     extensions: ['.js', '.json', '.jsx', '.mjs', '.ts', '.tsx', '.vue'],
   },
   build: {
-    outDir: 'public/build', // Changed this line
+    outDir: 'public/build', 
     assetsDir: '',
-    manifest: true,
-    chunkSizeWarningLimit: 1000, // Increased from default 500
+    manifest: 'manifest.json',
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['vue', 'v-calendar'], // Add other large dependencies here
-          // You can add more manual chunks as needed
+          // add more manual chunks as needed
         },
         chunkFileNames: 'js/[name]-[hash].js',
         entryFileNames: 'js/[name]-[hash].js',
